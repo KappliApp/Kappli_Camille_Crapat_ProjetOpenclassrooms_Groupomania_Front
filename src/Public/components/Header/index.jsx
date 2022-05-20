ba7e2e5876ee2fedf092d2c8d5f4db89ff0d6d46
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-05-20 16:52:38 +0200

    create Header

--- src/Public/components/Header/index.jsx
+++ src/Public/components/Header/index.jsx
@@ -1,9 +1,26 @@
 import styled from 'styled-components';
 
-const HeaderStyle = styled.header``;
+import colors from '../../../utils/styles/colors';
 
-function Header(){
-    <HeaderStyle></HeaderStyle>
+import Logo from '../../../assets/img/icon-left-font-monochrome-white.svg'
+
+const HeaderStyle = styled.header`
+background-color: ${colors.primary};
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 95px;`;
+
+const LogoStyle = styled.img`
+height: 65px;`
+
+function Header() { 
+    return(
+    <HeaderStyle>
+        <LogoStyle src={Logo} alt='Logo de la société Groupomania' />
+    </HeaderStyle>
+    )
 }
 
 export default Header;
