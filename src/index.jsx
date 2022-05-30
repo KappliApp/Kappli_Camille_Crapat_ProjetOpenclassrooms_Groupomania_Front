820a89db09d7a1540a9868c525fb74f7399b224c
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-05-30 16:15:41 +0200

    add fontawesome import

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import GlobalStyle from './utils/styles/base';
+import './utils/libs/fontawesome';
 
 import Public from './Public';
 import User from './User';
@@ -10,14 +11,14 @@ import Admin from './Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-    <GlobalStyle/>
-      <Routes>
-        <Route exact path="/*" element={ <Public /> } />
-        <Route exact path='/user/*' element={ <User/> }/>
-        <Route exact path='/admin/*' element={ <Admin/> } />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+   <React.StrictMode>
+      <Router>
+         <GlobalStyle />
+         <Routes>
+            <Route exact path="/*" element={<Public />} />
+            <Route exact path="/user/*" element={<User />} />
+            <Route exact path="/admin/*" element={<Admin />} />
+         </Routes>
+      </Router>
+   </React.StrictMode>
 );
