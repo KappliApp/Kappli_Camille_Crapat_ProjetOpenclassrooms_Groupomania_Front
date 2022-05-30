a8c7c1a9a79f1b05fc70a98df1b869530066b0ec
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-05-30 17:13:10 +0200

    delete footer components

--- src/Public/index.jsx
+++ src/Public/index.jsx
@@ -1,22 +1,19 @@
 import { Fragment } from 'react';
-import { Routes, Route, useLocation} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Header from './components/Header/index';
 import Login from './pages/Login';
-import Footer from './components/Footer';
 
-function Public(){
-    let path = useLocation().pathname;
-    console.log(path)
-return (
-    <Fragment>
-        <Header />
-        <Routes>
-            <Route path={path} element={ <Login/> } />
-        </Routes>
-        <Footer/>
-    </Fragment>
-)
+function Public() {
+   let path = useLocation().pathname;
+   return (
+      <Fragment>
+         <Header />
+         <Routes>
+            <Route path={path} element={<Login />} />
+         </Routes>
+      </Fragment>
+   );
 }
 
 export default Public;
