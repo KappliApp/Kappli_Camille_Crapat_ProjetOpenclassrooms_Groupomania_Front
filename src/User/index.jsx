bf9d9476e965248780c6c33362c0b512526e3d86
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-05-30 18:24:48 +0200

    add Header components

--- src/User/index.jsx
+++ src/User/index.jsx
@@ -1,5 +1,18 @@
-function User(){
+import { Fragment } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
+import Header from './components/Header';
+import AllPosts from './pages/AllPosts';
+
+function User() {
+   return (
+      <Fragment>
+         <Header />
+         <Routes>
+            <Route path="allposts" element={<AllPosts />} />
+         </Routes>
+      </Fragment>
+   );
 }
 
 export default User;
