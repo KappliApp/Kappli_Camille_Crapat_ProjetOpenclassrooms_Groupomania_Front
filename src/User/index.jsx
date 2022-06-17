15fcbc978a738244a595907943646b85f0224cb1
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-06-17 19:35:13 +0200

    create nav

--- src/User/index.jsx
+++ src/User/index.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Nav from './components/Nav';
 import AllPosts from './pages/AllPosts';
 
 function User() {
    return (
       <Fragment>
+         <Nav />
          <Routes>
             <Route path="allposts" element={<AllPosts />} />
          </Routes>
