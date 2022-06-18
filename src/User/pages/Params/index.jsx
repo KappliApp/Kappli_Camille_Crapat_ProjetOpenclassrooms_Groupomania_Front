964150cbfb6cb9f9fd2485b919de7cd11aeb7420
K@ppli - Camille CRAPAT <contact@kappli.eu>
2022-06-18 16:32:54 +0200

    change nav

--- src/User/pages/Params/index.jsx
+++ src/User/pages/Params/index.jsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+
+import colors from '../../../utils/styles/colors';
+
+const AllParamsStyle = styled.section`
+   width: 100%;
+   min-height: 78vh;
+   padding: 50px 0 50px 0;
+   background-color: ${colors.grey_light};
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+`;
+
 function Params({ user, setUser, token, setToken }) {
-   return <h2>Params</h2>;
+   return <AllParamsStyle>Params</AllParamsStyle>;
 }
 
 export default Params;
